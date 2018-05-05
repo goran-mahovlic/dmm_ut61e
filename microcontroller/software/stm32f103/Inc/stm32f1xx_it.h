9c46276f9e1331f984ff447ece91ba44a768bdad
Goran Mahovlic <goran.mahovlic@gmail.com>
2018-05-05 11:48:32 +0200

    Adding RTC

--- microcontroller/software/stm32f103/Inc/stm32f1xx_it.h
+++ microcontroller/software/stm32f103/Inc/stm32f1xx_it.h
@@ -56,6 +56,7 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void RTC_IRQHandler(void);
 void USB_LP_CAN1_RX0_IRQHandler(void);
 void USART1_IRQHandler(void);
 
@@ -63,7 +64,6 @@ void USART1_IRQHandler(void);
 char rxBuffer[100], meterDataReady, rxData[2];
 char rxIndex;
 /* USER CODE END 0 */
-
 #ifdef __cplusplus
 }
 #endif
